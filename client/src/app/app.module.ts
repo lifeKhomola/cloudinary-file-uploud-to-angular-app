ba8ba8471f1703f75e6ee56ee491776e77dafc0a
Vhutshilo  Khomola <vhutshilo.khomola@dadevs.co.za>
2022-02-03 12:21:38 +0200

    display all images from cloudify

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CloudinaryModule} from '@cloudinary/ng';
+import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageUploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CloudinaryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
